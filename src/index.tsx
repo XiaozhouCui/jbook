58e20f62732c39f827ecea55c74664527cdd3a58
Joe Cui <xiaozhou.cui@gmail.com>
2021-02-06 13:49:57 +1000

    Bundled js modules and css

--- src/index.tsx
+++ src/index.tsx
@@ -1,50 +1,42 @@
-import * as esbuild from "esbuild-wasm";
-import { useEffect, useState, useRef } from "react";
-import ReactDOM from "react-dom";
-import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
-
-// copy esbuild.wasm from node_modules to public folder
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () => {
   const ref = useRef<any>();
-  const [input, setInput] = useState("");
-  const [code, setCode] = useState("");
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
 
   const startService = async () => {
-    // assign the esbuild service to ref.current (useRef)
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: "/esbuild.wasm", // web assembly binary in public/ folder
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
     });
   };
-
   useEffect(() => {
     startService();
   }, []);
 
   const onClick = async () => {
-    if (!ref.current) return;
+    if (!ref.current) {
+      return;
+    }
 
-    // // transpile the input code (async)
-    // const result = await ref.current.transform(input, {
-    //   loader: "jsx",
-    //   target: "es2015",
-    // });
-
-    // bundle the input file
     const result = await ref.current.build({
-      entryPoints: ["index.js"],
+      entryPoints: ['index.js'],
       bundle: true,
       write: false,
-      plugins: [unpkgPathPlugin()],
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
     });
 
     // console.log(result);
-    
-    // // save the transpiled code to state
-    // setCode(result.code);
-    
-    // save the bundled code to state
+
     setCode(result.outputFiles[0].text);
   };
 
@@ -62,4 +54,4 @@ const App = () => {
   );
 };
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(<App />, document.querySelector('#root'));
--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,39 +1,28 @@
 import * as esbuild from "esbuild-wasm";
-import axios from "axios";
 
 export const unpkgPathPlugin = () => {
   return {
     name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
-      build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log("onResolve", args);
-        if (args.path === "index.js") {
-          return { path: args.path, namespace: "a" };
-        } else if (args.path === "tiny-test-pkg") {
-          return {
-            path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js",
-            namespace: "a",
-          };
-        }
+      // Handle root entry file of 'index.js'
+      build.onResolve({ filter: /(^index\.js$)/ }, () => {
+        return { path: "index.js", namespace: "a" };
       });
 
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log("onLoad", args);
-
-        if (args.path === "index.js") {
-          return {
-            loader: "jsx",
-            contents: `
-              const message = require('tiny-test-pkg');
-              console.log(message);
-            `,
-          };
-        }
-
-        const { data } = await axios.get(args.path);
+      // Handle relative paths in a module
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
         return {
-          loader: "jsx",
-          contents: data,
+          namespace: "a",
+          path: new URL(args.path, "https://unpkg.com" + args.resolveDir + "/")
+            .href,
+        };
+      });
+
+      // Handle main file of a module
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
+        return {
+          namespace: "a",
+          path: `https://unpkg.com/${args.path}`,
         };
       });
     },
