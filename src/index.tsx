152c0d5d816d0c37f5663dbc08206f260a97c562
Joe Cui <xiaozhou.cui@gmail.com>
2021-02-06 15:49:37 +1000

    Added an isolated iframe

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,18 @@
-import * as esbuild from 'esbuild-wasm';
-import { useState, useEffect, useRef } from 'react';
-import ReactDOM from 'react-dom';
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
-import { fetchPlugin } from './plugins/fetch-plugin';
+import * as esbuild from "esbuild-wasm";
+import { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
+import { fetchPlugin } from "./plugins/fetch-plugin";
 
 const App = () => {
   const ref = useRef<any>();
-  const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
 
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
+      wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
     });
   };
   useEffect(() => {
@@ -25,19 +25,26 @@ const App = () => {
     }
 
     const result = await ref.current.build({
-      entryPoints: ['index.js'],
+      entryPoints: ["index.js"],
       bundle: true,
       write: false,
       plugins: [unpkgPathPlugin(), fetchPlugin(input)],
       define: {
-        'process.env.NODE_ENV': '"production"',
-        global: 'window',
+        "process.env.NODE_ENV": '"production"',
+        global: "window",
       },
     });
 
     // console.log(result);
 
     setCode(result.outputFiles[0].text);
+
+    try {
+      // execute the entered code - DANGER
+      eval(result.outputFiles[0].text);
+    } catch (err) {
+      alert(err);
+    }
   };
 
   return (
@@ -50,8 +57,11 @@ const App = () => {
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
+      {/* execute user's code inside an iframe - SAFER for parent react app */}
+      {/* "sandbox" attr can disable direct js communication between parent and child iframe */}
+      <iframe sandbox="" title="test" src="/test.html" />
     </div>
   );
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
