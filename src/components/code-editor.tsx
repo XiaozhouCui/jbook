609370338ccc18a53319595a4312b4ae854487f7
Joe Cui <xiaozhou.cui@gmail.com>
2021-02-06 20:26:26 +1000

    Added bulma CSS theme

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -2,6 +2,7 @@ import { useRef } from "react";
 import MonacoEditor, { EditorDidMount } from "@monaco-editor/react";
 import prettier from "prettier";
 import parser from "prettier/parser-babel";
+import "./code-editor.css";
 
 interface CodeEditorProps {
   initialValue: string;
@@ -33,21 +34,28 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
     const unformatted = editorRef.current.getModel().getValue();
 
     // format that value
-    const formatted = prettier.format(unformatted, {
-      parser: "babel",
-      plugins: [parser],
-      useTabs: false,
-      semi: true,
-      singleQuote: true,
-    });
+    const formatted = prettier
+      .format(unformatted, {
+        parser: "babel",
+        plugins: [parser],
+        useTabs: false,
+        semi: true,
+        singleQuote: true,
+      })
+      .replace(/\n$/, ""); // remove the new line at the end of code
 
     // set the formatted value back in the editor
     editorRef.current.setValue(formatted);
   };
 
   return (
-    <div>
-      <button onClick={onFormatClick}>Format</button>
+    <div className="editor-wrapper">
+      <button
+        className="button button-format is-primary is-small"
+        onClick={onFormatClick}
+      >
+        Format
+      </button>
       <MonacoEditor
         editorDidMount={onEditorDidMount}
         value={initialValue}
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
-import * as esbuild from "esbuild-wasm";
-import { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
+import { useState, useEffect, useRef } from "react";
+import * as esbuild from "esbuild-wasm";
+import "bulmaswatch/superhero/bulmaswatch.min.css";
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 import { fetchPlugin } from "./plugins/fetch-plugin";
 import CodeEditor from "./components/code-editor";
