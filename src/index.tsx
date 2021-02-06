3822a7a9dcb90dcffad10e40106e43c24118569e
Joe Cui <xiaozhou.cui@gmail.com>
2021-02-06 16:19:13 +1000

    Executed the user input code inside iframe

--- src/index.tsx
+++ src/index.tsx
@@ -9,21 +9,24 @@ const App = () => {
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
 
+  // use esbuild to transpile and bundle user's code
   const startService = async () => {
+    // store the esbuild object into ref.current
     ref.current = await esbuild.startService({
       worker: true,
       wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
     });
   };
+
   useEffect(() => {
     startService();
   }, []);
 
   const onClick = async () => {
-    if (!ref.current) {
-      return;
-    }
+    // ref.current is the esbuild object
+    if (!ref.current) return;
 
+    // get bundled code result from esbuild
     const result = await ref.current.build({
       entryPoints: ["index.js"],
       bundle: true,
@@ -35,17 +38,16 @@ const App = () => {
       },
     });
 
-    // console.log(result);
-
+    // store the bundled code into state
     setCode(result.outputFiles[0].text);
-
-    try {
-      // execute the entered code - DANGER
-      eval(result.outputFiles[0].text);
-    } catch (err) {
-      alert(err);
-    }
   };
+
+  // prepare the bundled code to be executed in iframe
+  const html = `
+    <script>
+      ${code}
+    </script>
+  `;
 
   return (
     <div>
@@ -59,7 +61,7 @@ const App = () => {
       <pre>{code}</pre>
       {/* execute user's code inside an iframe - SAFER for parent react app */}
       {/* "sandbox" attr can disable direct js communication between parent and child iframe */}
-      <iframe sandbox="" title="test" src="/test.html" />
+      <iframe title="sandbox" sandbox="allow-scripts" srcDoc={html} />
     </div>
   );
 };
