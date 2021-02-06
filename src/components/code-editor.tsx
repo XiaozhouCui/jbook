a53438f15eb4b327631b4d39dad1042350370f4d
Joe Cui <xiaozhou.cui@gmail.com>
2021-02-06 18:04:26 +1000

    Adding in-browser code editor: Monaco

---
+++ src/components/code-editor.tsx
@@ -0,0 +1,23 @@
+import MonacoEditor from "@monaco-editor/react";
+
+const CodeEditor = () => {
+  return (
+    <MonacoEditor
+      theme="dark"
+      language="javascript"
+      height="500px"
+      options={{
+        wordWrap: "on",
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
+      }}
+    />
+  );
+};
+
+export default CodeEditor;
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 import { fetchPlugin } from "./plugins/fetch-plugin";
+import CodeEditor from "./components/code-editor";
 
 const App = () => {
   const ref = useRef<any>();
@@ -69,6 +70,7 @@ const App = () => {
 
   return (
     <div>
+      <CodeEditor />
       <textarea
         value={input}
         onChange={(e) => setInput(e.target.value)}
