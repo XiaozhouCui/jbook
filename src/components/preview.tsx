b1bccd6d26f2ea1eccd56dac1f7e6e29d5978312
Joe Cui <xiaozhou.cui@gmail.com>
2021-02-07 20:20:49 +1000

    Handle async error in Preview

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -16,13 +16,24 @@ const html = `
         <div id="root"></div>
       </body>
       <script>
+        const handleError = (err) => {
+          const root = document.querySelector('#root');
+          root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+          console.error(err);
+        };
+
+        // catch async error
+        window.addEventListener('error', (event) => {
+          event.preventDefault(); // error will be printed in handleError()
+          handleError(event.error);
+        });
+
         window.addEventListener('message', (event) => {
           try {
             eval(event.data);
           } catch (err) {
-            const root = document.querySelector('#root');
-            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
-            console.error(err);
+            // catch sync error
+            handleError(err);
           }
         }, false)
       </script>
