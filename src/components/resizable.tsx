0246467f90a735c0f01a1a0ac4878db51788e2d0
Joe Cui <xiaozhou.cui@gmail.com>
2021-02-07 16:07:16 +1000

    Synchronizing width state

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { ResizableBox, ResizableBoxProps } from "react-resizable";
 import "./resizable.css";
 
@@ -8,19 +9,48 @@ interface ResizableProps {
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
   let resizableProps: ResizableBoxProps;
 
+  const [innerHeight, setInnerHeight] = useState(window.innerHeight);
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+  const [width, setWidth] = useState(window.innerWidth * 0.75);
+
+  useEffect(() => {
+    let timer: any;
+    const listener = () => {
+      if (timer) clearTimeout(timer);
+      timer = setTimeout(() => {
+        // when resizing browser window, dynamically save the current dimensions.
+        // the code editor will auto resize 100 milliseconds after browser window is resized
+        setInnerHeight(window.innerHeight);
+        setInnerWidth(window.innerWidth);
+        if (window.innerWidth * 0.75 < width) {
+          setWidth(window.innerWidth * 0.75);
+        }
+      }, 100);
+    };
+    window.addEventListener("resize", listener);
+
+    return () => {
+      window.removeEventListener("resize", listener);
+    };
+  }, [width]);
+
   if (direction === "horizontal") {
     resizableProps = {
       className: "resize-horizontal",
-      minConstraints: [window.innerWidth * 0.2, Infinity],
-      maxConstraints: [window.innerWidth * 0.75, Infinity],
-      width: window.innerWidth * 0.75,
+      minConstraints: [innerWidth * 0.2, Infinity],
+      maxConstraints: [innerWidth * 0.75, Infinity],
+      width: width,
       height: Infinity,
       resizeHandles: ["e"],
+      onResizeStop: (event, data) => {
+        // when stopped draging, grab the ResizableBox width
+        setWidth(data.size.width);
+      },
     };
   } else {
     resizableProps = {
       minConstraints: [Infinity, 24],
-      maxConstraints: [Infinity, window.innerHeight * 0.9],
+      maxConstraints: [Infinity, innerHeight * 0.9],
       width: Infinity,
       height: 300,
       resizeHandles: ["s"],
