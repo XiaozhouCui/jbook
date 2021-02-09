cc976b31c14b903f3d557ce1f49c44813d210a0d
Joe Cui <xiaozhou.cui@gmail.com>
2021-02-09 12:17:37 +1000

    Adding Action Creators

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,17 @@ import ReactDOM from "react-dom";
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 // import CodeCell from "./components/code-cell";
 import TextEditor from "./components/text-editor";
+import { Provider } from "react-redux";
+import { store } from "./state";
 
 const App = () => {
   return (
-    <div>
-      {/* Each CodeCell has 1 CodeEditor (monaco) and 1 Preview window (iframe) */}
-      <TextEditor />
-    </div>
+    <Provider store={store}>
+      <div>
+        {/* Each CodeCell has 1 CodeEditor (monaco) and 1 Preview window (iframe) */}
+        <TextEditor />
+      </div>
+    </Provider>
   );
 };
 
--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,5 +1,42 @@
-const index = () => {
+import { CellTypes } from "./../cell";
+import { ActionType } from "../action-types";
+import {
+  UpdateCellAction,
+  MoveCellAction,
+  DeleteCellAction,
+  InsertCellBeforeAction,
+} from "./../actions/index";
 
-}
+export const updateCell = (id: string, content: string): UpdateCellAction => {
+  return {
+    type: ActionType.UPDATE_CELL,
+    payload: { id, content },
+  };
+};
 
-export default index
+export const deleteCell = (id: string): DeleteCellAction => {
+  return {
+    type: ActionType.DELETE_CELL,
+    payload: id,
+  };
+};
+
+export const moveCell = (
+  id: string,
+  direction: "up" | "down"
+): MoveCellAction => {
+  return {
+    type: ActionType.MOVE_CELL,
+    payload: { id, direction },
+  };
+};
+
+export const insertCellBefore = (
+  id: string,
+  cellType: CellTypes
+): InsertCellBeforeAction => {
+  return {
+    type: ActionType.INCERT_CELL_BEFORE,
+    payload: { id, type: cellType },
+  };
+};
