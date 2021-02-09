bcd1556ddf6c9d6a70ddf927521a92f99519c8b8
Joe Cui <xiaozhou.cui@gmail.com>
2021-02-09 15:38:14 +1000

    Use "immer" to handle reducer logics

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -1,3 +1,4 @@
+import produce from "immer";
 import { Cell } from "../cell";
 import { Action } from "../actions";
 import { ActionType } from "../action-types";
@@ -18,32 +19,31 @@ const initialState: CellsState = {
   data: {},
 };
 
-const reducer = (
-  state: CellsState = initialState,
-  action: Action
-): CellsState => {
-  switch (action.type) {
-    case ActionType.UPDATE_CELL:
-      const { id, content } = action.payload;
-      return {
-        ...state,
-        data: {
-          ...state.data,
-          [id]: {
-            ...state.data[id],
-            content,
-          },
-        },
-      };
-    case ActionType.DELETE_CELL:
-      return state;
-    case ActionType.MOVE_CELL:
-      return state;
-    case ActionType.INCERT_CELL_BEFORE:
-      return state;
-    default:
-      return state;
+const reducer = produce(
+  (state: CellsState = initialState, action: Action): CellsState | void => {
+    switch (action.type) {
+      case ActionType.UPDATE_CELL:
+        const { id, content } = action.payload;
+        // Thanks to "immer" produce(), no need to return a cloned object
+        state.data[id].content = content;
+        return;
+
+        // return {
+        //   ...state,
+        //   data: { ...state.data, [id]: { ...state.data[id], content } },
+        // };
+      case ActionType.DELETE_CELL:
+        delete state.data[action.payload];
+        state.order = state.order.filter((id) => id !== action.payload);
+        return;
+      case ActionType.MOVE_CELL:
+        return state;
+      case ActionType.INCERT_CELL_BEFORE:
+        return state;
+      default:
+        return state;
+    }
   }
-};
+);
 
 export default reducer;
