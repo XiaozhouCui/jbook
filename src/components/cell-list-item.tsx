cd9df3c24dcdae0293fb3a608f927b3ce1a7e97f
Joe Cui <xiaozhou.cui@gmail.com>
2021-02-10 16:57:17 +1000

    Connecting redux to TextEditor

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -12,7 +12,7 @@ const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
   if (cell.type === "code") {
     child = <CodeCell cell={cell} />;
   } else {
-    child = <TextEditor />;
+    child = <TextEditor cell={cell} />;
   }
   return <div>{child}</div>;
 };
--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,10 +1,17 @@
 import { useEffect, useRef, useState } from "react";
 import MDEditor from "@uiw/react-md-editor";
 import "./text-editor.css";
+import { Cell } from "../state";
+import { useActions } from "../hooks/use-actions";
 
-const TextEditor: React.FC = () => {
+interface TextEditorProps {
+  cell: Cell;
+}
+
+const TextEditor: React.FC<TextEditorProps> = ({ cell }) => {
   const [editing, setEditing] = useState(false);
-  const [value, setValue] = useState("# Header");
+
+  const { updateCell } = useActions();
 
   const ref = useRef<HTMLDivElement | null>(null);
 
@@ -35,7 +42,10 @@ const TextEditor: React.FC = () => {
   if (editing) {
     return (
       <div className="text-editor" ref={ref}>
-        <MDEditor value={value} onChange={(v) => setValue(v || '')} />
+        <MDEditor
+          value={cell.content}
+          onChange={(v) => updateCell(cell.id, v || "")}
+        />
       </div>
     );
   }
@@ -44,7 +54,7 @@ const TextEditor: React.FC = () => {
   return (
     <div className="text-editor card" onClick={() => setEditing(true)}>
       <div className="card-content">
-        <MDEditor.Markdown source={value} />
+        <MDEditor.Markdown source={cell.content || "Click to edit"} />
       </div>
     </div>
   );
