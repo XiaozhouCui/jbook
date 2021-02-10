a9f6f3264137c180822d7a410722660c87d850e7
Joe Cui <xiaozhou.cui@gmail.com>
2021-02-10 19:39:40 +1000

    Added styles for ActionBar

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -3,7 +3,7 @@ import { Cell } from "../state";
 import ActionBar from "./action-bar";
 import CodeCell from "./code-cell";
 import TextEditor from "./text-editor";
-import "./cell-list-item.css"
+import "./cell-list-item.css";
 
 interface CellListItemProps {
   cell: Cell;
@@ -12,17 +12,24 @@ interface CellListItemProps {
 const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
   let child: JSX.Element;
   if (cell.type === "code") {
-    // Each CodeCell has 1 CodeEditor (monaco) and 1 Preview window (iframe)    
-    child = <CodeCell cell={cell} />;
+    // Each CodeCell has 1 CodeEditor (monaco) and 1 Preview window (iframe)
+    child = (
+      <>
+        <div className="action-bar-wrapper">
+          <ActionBar id={cell.id} />
+        </div>
+        <CodeCell cell={cell} />
+      </>
+    );
   } else {
-    child = <TextEditor cell={cell} />;
+    child = (
+      <>
+        <TextEditor cell={cell} />
+        <ActionBar id={cell.id} />
+      </>
+    );
   }
-  return (
-    <div className="cell-list-item">
-      {child}
-      <ActionBar id={cell.id} />
-    </div>
-  );
+  return <div className="cell-list-item">{child}</div>;
 };
 
 export default CellListItem;
